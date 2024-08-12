2e4899dfe6c3464cabb8887e8dac188306949b18
Chengshun Shang <chengshun.shang1996@gmail.com>
2024-08-12 17:33:31 +0200

    save files

--- applications/DEMApplication/custom_constitutive/DEM_P_D_Linear_viscous_Coulomb_CL.cpp
+++ applications/DEMApplication/custom_constitutive/DEM_P_D_Linear_viscous_Coulomb_CL.cpp
@@ -73,6 +73,61 @@ namespace Kratos {
 
         KRATOS_TRY
 
+        Vector3 relVel = Vector3(elem1ContactPointVelX, elem1ContactPointVelY, elem1ContactPointVelZ) -
+        Vector3(elem2ContactPointVelX, elem2ContactPointVelY, elem2ContactPointVelZ);
+
+        // Put the values into a more useful form
+        Vector3 angVel1(elem1AngVelX, elem1AngVelY, elem1AngVelZ);
+        Vector3 angVel2(elem2AngVelX, elem2AngVelY, elem2AngVelZ);
+        CSimple3DPoint  contactPoint(contactPointX, contactPointY, contactPointZ);
+
+        // The unit vector from element 1 to the contact point
+        Vector3 unitCPVect = contactPoint - CSimple3DPoint(elem1PosX, elem1PosY, elem1PosZ);
+        unitCPVect.normalise();
+
+        // normal and tangential components of the relative velocity at the contact point
+        Vector3 relVel_n = unitCPVect * unitCPVect.dot(relVel);
+        Vector3 relVel_t = relVel - relVel_n;
+
+        // Damping calculation
+        double B = 0.0;
+        if (coeffRest > 0.0)
+        {
+            double myLog = log(coeffRest);
+            B = -myLog / sqrt(myLog * myLog + PI * PI);
+        }
+
+        double S_n = 2.0 * nEquivYoungsMod * sqrt(nEquivRadius * normalPhysicalOverlap);
+        Vector3 F_nd = unitCPVect * 2 * sqrt(5.0 / 6.0) * B * sqrt(S_n * nEquivMass) *  relVel_n.length();
+
+        // Are we in a loading situation?
+        if (relVel_n.dot(unitCPVect) > 0.0)
+        {
+            F_nd = -F_nd;
+        }
+
+        double S_t = 8.0 * nEquivShearMod * sqrt(nEquivRadius * normalPhysicalOverlap);
+        Vector3 nOverlap_t(tangentialPhysicalOverlapX, tangentialPhysicalOverlapY, tangentialPhysicalOverlapZ);
+        Vector3 F_t = -nOverlap_t * S_t;
+
+        // Damping
+        Vector3 F_td, newF_t;
+
+        if (F_t.length() > F_n.length() * (staticFriction))
+        {
+            newF_t = F_t * F_n.length() * (staticFriction) / F_t.length();
+            nOverlap_t = -newF_t / S_t; //slippage has occurred so the tangential overlap is reduced a bit
+
+            //at this point we get energy loss from the sliding!
+            F_td = newF_t;
+        }
+        else
+        {
+            //at this point we get energy loss from the damping!
+            F_td = -relVel_t * 2 * sqrt(5.0 / 6.0) * B * sqrt(S_t * nEquivMass);
+            newF_t = F_t + F_td;
+        }
+
         //double kn = 100000.0;
         const double kn = (*mpProperties)[CONTACT_K_N];
 		contact_force = mOverlapVector * kn;
