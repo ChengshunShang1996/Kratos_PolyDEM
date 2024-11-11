37847e3eb0255a4806dd8ea25c3a66b003a7a6ae
Chengshun Shang <chengshun.shang1996@gmail.com>
2024-11-11 10:21:34 +0100

    improve the vtk output

--- applications/DEMApplication/python_scripts/polyhedron_vtk_output.py
+++ applications/DEMApplication/python_scripts/polyhedron_vtk_output.py
@@ -36,6 +36,26 @@ class PolyhedronVtkOutput():
 
     def DataPreparation(self):
 
+        self.find_smp_p = False
+        for smp in self.polyhedron_model_part.SubModelParts:
+            if smp.Name == "DEMParts_Body":
+                self.polyhedron_model_part_p = smp
+                self.find_smp_p = True
+
+        self.find_smp_w = False
+        for smp in self.polyhedron_model_part.SubModelParts:
+            if smp.Name == "DEMWall":
+                self.polyhedron_model_part_w = smp
+                self.find_smp_w = True
+        
+        if self.find_smp_p and self.find_smp_w:
+            self.DataPreparationParticles()
+            self.DataPreparationWalls()
+        else:
+            self.DataPreparationAll()
+
+    def DataPreparationAll(self):
+
         number_of_nodes = self.polyhedron_model_part.NumberOfNodes(0)
 
         self.polygon_centers = np.zeros((number_of_nodes, 3))
@@ -77,10 +97,96 @@ class PolyhedronVtkOutput():
             for node in self.polyhedron_model_part.Nodes:
                 self.polygon_displacement[i] = [node.GetSolutionStepValue(DISPLACEMENT_X), node.GetSolutionStepValue(DISPLACEMENT_Y), node.GetSolutionStepValue(DISPLACEMENT_Z)]
                 i += 1
-    
-    def WriteResults(self, poly_output_cnt):
 
-        self.DataPreparation()
+    def DataPreparationParticles(self):
+        
+        number_of_nodes_p = self.polyhedron_model_part_p.NumberOfNodes(0)
+
+        self.polygon_centers_p = np.zeros((number_of_nodes_p, 3))
+        self.polygon_origins_p = []
+        self.polyhedron_faces_p = []
+        self.polygon_velocity_p = np.zeros((number_of_nodes_p, 3))
+        self.polygon_displacement_p = np.zeros((number_of_nodes_p, 3))
+
+        i = 0
+        for node in self.polyhedron_model_part_p.Nodes:
+            self.polygon_centers_p[i] = [node.X, node.Y, node.Z]
+            i += 1
+
+        for element in self.polyhedron_model_part_p.Elements:
+            vertices = element.GetListOfVertices()
+            temp_vertices_list = []
+            for vertex in vertices:
+                temp_vertices_list.append([vertex[0], vertex[1], vertex[2]])
+            self.polygon_origins_p.append(temp_vertices_list)
+
+        for element in self.polyhedron_model_part_p.Elements:
+            faces = element.GetListOfFaces()
+            temp_faces_list = []
+            for face in faces:
+                temp_face_sub = []
+                for face_sub in face:
+                    temp_face_sub.append(face_sub)
+                temp_faces_list.append(temp_face_sub)
+            self.polyhedron_faces_p.append(temp_faces_list)
+
+        if self.PostVelocity:
+            i = 0
+            for node in self.polyhedron_model_part_p.Nodes:
+                self.polygon_velocity_p[i] = [node.GetSolutionStepValue(VELOCITY_X), node.GetSolutionStepValue(VELOCITY_Y), node.GetSolutionStepValue(VELOCITY_Z)]
+                i += 1
+
+        if self.PostDisplacement:
+            i = 0
+            for node in self.polyhedron_model_part_p.Nodes:
+                self.polygon_displacement_p[i] = [node.GetSolutionStepValue(DISPLACEMENT_X), node.GetSolutionStepValue(DISPLACEMENT_Y), node.GetSolutionStepValue(DISPLACEMENT_Z)]
+                i += 1
+
+    def DataPreparationWalls(self):
+        
+        number_of_nodes_w = self.polyhedron_model_part_w.NumberOfNodes(0)
+
+        self.polygon_centers_w = np.zeros((number_of_nodes_w, 3))
+        self.polygon_origins_w = []
+        self.polyhedron_faces_w = []
+        self.polygon_velocity_w = np.zeros((number_of_nodes_w, 3))
+        self.polygon_displacement_w = np.zeros((number_of_nodes_w, 3))
+
+        i = 0
+        for node in self.polyhedron_model_part_w.Nodes:
+            self.polygon_centers_w[i] = [node.X, node.Y, node.Z]
+            i += 1
+
+        for element in self.polyhedron_model_part_w.Elements:
+            vertices = element.GetListOfVertices()
+            temp_vertices_list = []
+            for vertex in vertices:
+                temp_vertices_list.append([vertex[0], vertex[1], vertex[2]])
+            self.polygon_origins_w.append(temp_vertices_list)
+        
+        for element in self.polyhedron_model_part_w.Elements:
+            faces = element.GetListOfFaces()
+            temp_faces_list = []
+            for face in faces:
+                temp_face_sub = []
+                for face_sub in face:
+                    temp_face_sub.append(face_sub)
+                temp_faces_list.append(temp_face_sub)
+            self.polyhedron_faces_w.append(temp_faces_list)
+        
+        if self.PostVelocity:
+            i = 0
+            for node in self.polyhedron_model_part_w.Nodes:
+                self.polygon_velocity_w[i] = [node.GetSolutionStepValue(VELOCITY_X), node.GetSolutionStepValue(VELOCITY_Y), node.GetSolutionStepValue(VELOCITY_Z)]
+                i += 1
+
+        if self.PostDisplacement:
+            i = 0
+            for node in self.polyhedron_model_part_w.Nodes:
+                self.polygon_displacement_w[i] = [node.GetSolutionStepValue(DISPLACEMENT_X), node.GetSolutionStepValue(DISPLACEMENT_Y), node.GetSolutionStepValue(DISPLACEMENT_Z)]
+                i += 1     
+    
+    def WriteOutAll(self, poly_output_cnt):
         
         points_vtk = vtk.vtkPoints()
         cells_vtk = vtk.vtkCellArray()
@@ -149,4 +255,156 @@ class PolyhedronVtkOutput():
         writer.SetFileName(file_path)
         writer.SetInputData(grid)
         writer.Write()
+
+    def WriteOutParticles(self, poly_output_cnt):
+        
+        points_vtk = vtk.vtkPoints()
+        cells_vtk = vtk.vtkCellArray()
+
+        polyhedron_points = []
+        i = 0
+        for polygon_center in self.polygon_centers_p:
+            polygon = polygon_center + self.polygon_origins_p[i]
+            for poly in polygon:
+                polyhedron_points.append(poly)
+            i += 1
+
+        polyhedron_faces = []
+        i = 0
+        offset = 0
+        for polyhedron_face in self.polyhedron_faces_p:
+            if i > 0:
+                offset += len(self.polygon_origins_p[i-1])
+            for face in polyhedron_face:
+                adjusted_face = [v + offset for v in face]
+                polyhedron_faces.append(adjusted_face)
+            i += 1
+        
+        for point_coord in polyhedron_points:
+            points_vtk.InsertNextPoint(point_coord)
+
+        for face in polyhedron_faces:
+            cell = vtk.vtkPolygon()
+            cell.GetPointIds().SetNumberOfIds(len(face))
+            for j, point_id in enumerate(face):
+                cell.GetPointIds().SetId(j, point_id)
+            cells_vtk.InsertNextCell(cell)
+
+        grid = vtk.vtkUnstructuredGrid()
+        grid.SetPoints(points_vtk)
+        grid.SetCells(vtk.VTK_POLYGON, cells_vtk)
+
+        if self.PostVelocity:
+            velocity_array = vtk.vtkDoubleArray()
+            velocity_array.SetName("Velocity")
+            velocity_array.SetNumberOfComponents(3)
+
+            i = 0
+            for ii in range(len(self.polygon_centers_p)):
+                for jj in range(len(self.polygon_origins_p[i])):
+                    velocity_array.InsertNextTuple(self.polygon_velocity_p[i])
+                i += 1
+
+            grid.GetPointData().AddArray(velocity_array)
+
+        if self.PostDisplacement:
+            displacement_array = vtk.vtkDoubleArray()
+            displacement_array.SetName("Displacement")
+            displacement_array.SetNumberOfComponents(3)
+
+            i = 0
+            for ii in range(len(self.polygon_centers_p)):
+                for jj in range(len(self.polygon_origins_p[i])):
+                    displacement_array.InsertNextTuple(self.polygon_displacement_p[i])
+                i += 1
+
+            grid.GetPointData().AddArray(displacement_array)
+
+        writer = vtk.vtkXMLUnstructuredGridWriter()
+        file_path = os.path.join(self.vtk_post_path_directory, "polyhedron_p_{}.vtu".format(poly_output_cnt))
+        writer.SetFileName(file_path)
+        writer.SetInputData(grid)
+        writer.Write()
+
+    def WriteOutWalls(self, poly_output_cnt):
+        
+        points_vtk = vtk.vtkPoints()
+        cells_vtk = vtk.vtkCellArray()
+
+        polyhedron_points = []
+        i = 0
+        for polygon_center in self.polygon_centers_w:
+            polygon = polygon_center + self.polygon_origins_w[i]
+            for poly in polygon:
+                polyhedron_points.append(poly)
+            i += 1
+
+        polyhedron_faces = []
+        i = 0
+        offset = 0
+        for polyhedron_face in self.polyhedron_faces_w:
+            if i > 0:
+                offset += len(self.polygon_origins_w[i-1])
+            for face in polyhedron_face:
+                adjusted_face = [v + offset for v in face]
+                polyhedron_faces.append(adjusted_face)
+            i += 1
+        
+        for point_coord in polyhedron_points:
+            points_vtk.InsertNextPoint(point_coord)
+
+        for face in polyhedron_faces:
+            cell = vtk.vtkPolygon()
+            cell.GetPointIds().SetNumberOfIds(len(face))
+            for j, point_id in enumerate(face):
+                cell.GetPointIds().SetId(j, point_id)
+            cells_vtk.InsertNextCell(cell)
+
+        grid = vtk.vtkUnstructuredGrid()
+        grid.SetPoints(points_vtk)
+        grid.SetCells(vtk.VTK_POLYGON, cells_vtk)
+
+        if self.PostVelocity:
+            velocity_array = vtk.vtkDoubleArray()
+            velocity_array.SetName("Velocity")
+            velocity_array.SetNumberOfComponents(3)
+
+            i = 0
+            for ii in range(len(self.polygon_centers_w)):
+                for jj in range(len(self.polygon_origins_w[i])):
+                    velocity_array.InsertNextTuple(self.polygon_velocity_w[i])
+                i += 1
+
+            grid.GetPointData().AddArray(velocity_array)
+
+        if self.PostDisplacement:
+            displacement_array = vtk.vtkDoubleArray()
+            displacement_array.SetName("Displacement")
+            displacement_array.SetNumberOfComponents(3)
+
+            i = 0
+            for ii in range(len(self.polygon_centers_w)):
+                for jj in range(len(self.polygon_origins_w[i])):
+                    displacement_array.InsertNextTuple(self.polygon_displacement_w[i])
+                i += 1
+
+            grid.GetPointData().AddArray(displacement_array)
+
+        writer = vtk.vtkXMLUnstructuredGridWriter()
+        file_path = os.path.join(self.vtk_post_path_directory, "polyhedron_w_{}.vtu".format(poly_output_cnt))
+        writer.SetFileName(file_path)
+        writer.SetInputData(grid)
+        writer.Write()
+    
+    def WriteResults(self, poly_output_cnt):
+
+        self.DataPreparation()
+
+        if self.find_smp_p and self.find_smp_w:
+            self.WriteOutParticles(poly_output_cnt)
+            self.WriteOutWalls(poly_output_cnt)
+        else:
+            self.WriteOutAll(poly_output_cnt)
+        
+        
     
