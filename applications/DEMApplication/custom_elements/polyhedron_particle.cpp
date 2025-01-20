ca1122a3d5da063775045ce1ed6c9cb8df7943a9
Chengshun Shang <chengshun.shang1996@gmail.com>
2025-01-20 09:44:12 +0100

    clean up

--- applications/DEMApplication/custom_elements/polyhedron_particle.cpp
+++ applications/DEMApplication/custom_elements/polyhedron_particle.cpp
@@ -168,20 +168,8 @@ namespace Kratos {
                     mListOfVertices[i][2] = it->GetGeometry()[i].Coordinates()[2];
                 }
 
-                /*array_1d<double, 3> center = ZeroVector(3);
-                for (int i = 0; i < (int)mListOfVertices.size(); i++) {
-                    center[0] += mListOfVertices[i][0];
-                    center[1] += mListOfVertices[i][1];
-                    center[2] += mListOfVertices[i][2];
-                }
-                center[0] /= (int)mListOfVertices.size();
-                center[1] /= (int)mListOfVertices.size();
-                center[2] /= (int)mListOfVertices.size();
-
-                KRATOS_ERROR_IF_NOT(mListOfVertices.size()) << "The number of vertices is zero." << std::endl;*/
                 array_1d<double, 3> center = it->GetGeometry().Center();
 
-                // Calculate the new vertices
                 for (int i = 0; i < (int)mListOfVertices.size(); i++) {
                     mListOfVertices[i][0] -= center[0];
                     mListOfVertices[i][1] -= center[1];
@@ -256,17 +244,6 @@ namespace Kratos {
                     mListOfVertices[i][2] = it->GetGeometry()[i].Coordinates()[2];
                 }
                 
-                /*array_1d<double, 3> center = ZeroVector(3);
-                for (int i = 0; i < (int)mListOfVertices.size(); i++) {
-                    center[0] += mListOfVertices[i][0];
-                    center[1] += mListOfVertices[i][1];
-                    center[2] += mListOfVertices[i][2];
-                }
-                center[0] /= (int)mListOfVertices.size();
-                center[1] /= (int)mListOfVertices.size();
-                center[2] /= (int)mListOfVertices.size();
-
-                KRATOS_ERROR_IF_NOT(mListOfVertices.size()) << "The number of vertices is zero." << std::endl;*/
                 array_1d<double, 3> center = it->GetGeometry().Center();
 
                 for (int i = 0; i < (int)mListOfVertices.size(); i++) {
